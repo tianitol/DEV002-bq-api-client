77828a21be312e43953f198e1a816aa6fce29a4b
Nicole Henríquez <ale.nicolehs@gmail.com>
2023-03-27 13:55:47 -0300

    se agrega el error notfound 404 en vista

--- src/vistas/Login.jsx
+++ src/vistas/Login.jsx
@@ -4,6 +4,8 @@ function Login(props){
     const { setUser } = props;
 
     function login() {
+        //aquí en vez de setear el user en true al apretar el boton login, puede crearse
+        //una pormesa que al resolverse genere el login (esperar usuario y contraseña correctos)
         setUser(true);
     }
     return (
